7643b1982a60ee27a0ef8e77a93e449c2b3fd532
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-11-22 15:02:30 -0300

    Requisito 5

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
+import {vi} from 'vitest'
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Teste do componente <App />', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('Verifica se o formulário de pesquisa é renderizado na tela', () => {
+    expect(screen.getByTestId('name-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('column-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('comparison-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('value-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('button-filter')).toBeInTheDocument();
+  });
+
+  it('Verifica se a tabela é renderizada na tela', () => {
+    expect(screen.getByRole('table')).toBeInTheDocument();
+  });
+
+  it('Verifica se a tabela contém os valores corretos', () => {
+    const columnHeaders = [
+      'Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited', 'URL',
+    ];
+
+    columnHeaders.forEach((header) => {
+      expect(screen.getByRole('table')).toHaveTextContent(header);
+    });
+  });
 });
